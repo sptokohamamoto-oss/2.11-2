de42ba4438db0ce51f828b6af6d543dab6145953
冬桜子 濱元 <toko0124615@icloud.com>
2026-01-08 15:45:41 +0900

    a

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number))
+        { 
+
+            switch(number)
+            {
+
+                case 1:
+                    Console.WriteLine("月曜日");
+                    break;
+                case 2:
+                    Console.WriteLine("火曜日");
+                    break;
+                case 3:
+                    Console.WriteLine("水曜日");
+                    break;
+                case 4:
+                    Console.WriteLine("木曜日");
+                    break;
+                case 5:
+                    Console.WriteLine("金曜日");
+                    break;
+                case 6:
+                    Console.WriteLine("土曜日");
+                    break;
+                case 7:
+                    Console.WriteLine("日曜日");
+                    break;
+                default:
+                    Console.WriteLine("不正な入力です");
+                    break;
+            }
+
+
+        }
+    }
+}
+
